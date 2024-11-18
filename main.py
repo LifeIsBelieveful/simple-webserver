eca89ddbc67db5c5f066f2684d4478cabeb65df9
daniel <daniel-lee>
2024-11-18 10:50:10 +0900

    초기 앱 등록

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from routers import webhook
 
 app = FastAPI()
+app.include_router(webhook.router)
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ routers/webhook.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+
+from fastapi import APIRouter, HTTPException, Request, BackgroundTasks
+import requests
+from common.consts import DOORAY_ENDPOINT, DOORAY_API_KEY, DAYOFF_CAL_ID
+
+router = APIRouter(prefix='/webhook', tags=['webhook'])
+
+# Dooray API 호출 함수
+def call_dooray_api(time_min: str, time_max: str, calendar_id: str):
+    url = f"{DOORAY_ENDPOINT}/calendar/v1/calendars/*/events"
+    params = {
+        "timeMin": time_min,
+        "timeMax": time_max,
+        "calendars": calendar_id
+    }
+    headers = {
+        'Authorization': f'dooray-api {DOORAY_API_KEY}'
+    }
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=f"Dooray API call failed: {response.text}")
+    return response.json()
+
+@router.get("/calendar")
+async def get_calendar():
+    try:
+        # 기본값 설정
+        today = datetime.now()
+        time_min = today.strftime("%Y-%m-%dT%H:%M:%S+09:00")
+        time_max = (today + timedelta(days=7)).strftime("%Y-%m-%dT%H:%M:%S+09:00")
+
+        # Dooray API 호출 작업을 백그라운드에서 실행
+        data = call_dooray_api(time_min, time_max, DAYOFF_CAL_ID)
+
+        return {
+            "data": data
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing request: {str(e)}")
